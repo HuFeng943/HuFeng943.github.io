ca024284dfa4df4412f966c973d18ada06fe1856
HuFeng <chenzr233@gmail.com>
2025-08-03 15:56:07 +0800

    Update at 2025-08-03 15:56:06

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,10 @@ Write-Host "----------------------------------------"
 git submodule update --remote # 更新更新下主题
 git submodule foreach --recursive "git reset --hard && git clean -fdx" # 删掉多余的主题文件
 # 将当前目录下的所有修改提交并推送到 GitHub Pages
-Remove-Item -Path ".\public" -Recurse -Force  # 删除 public 目录
+Write-Host "----------------------------------------"
+Write-Host "正在删除本地构建文件..."
+Write-Host "----------------------------------------"
+Remove-Item -Path ".\public" -Recurse -Force -ErrorAction SilentlyContinue  # 删除 public 目录
 # 获取当前日期和时间，并格式化
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $commitMessage = "Update at $timestamp"
@@ -14,9 +17,8 @@ Write-Host "----------------------------------------"
 Write-Host "正在同步并部署网站..."
 Write-Host "提交信息: $commitMessage"
 Write-Host "----------------------------------------"
+git add . # 把所有修改都添加到暂存区
 git pull --rebase # 同步远程仓库并 rebase，避免产生合并提交
-# 提交所有本地修改
-git add .
 git commit -m $commitMessage
 # 推送本地修改到远程仓库
 git push
