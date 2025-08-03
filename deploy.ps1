195ea7a9540d473460f4f0424318cddf2a774d64
HuFeng <chenzr233@gmail.com>
2025-08-03 15:45:05 +0800

    Update at 2025-08-03 15:45:04

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,11 @@
 # 先更新下主题
-git submodule update --remote --merge
+Write-Host "----------------------------------------"
+Write-Host "正在更新主题文件..."
+Write-Host "----------------------------------------"
+git submodule update --remote # 更新更新下主题
+git submodule foreach --recursive "git reset --hard && git clean -fdx" # 删掉多余的主题文件
 # 将当前目录下的所有修改提交并推送到 GitHub Pages
-
+Remove-Item -Path ".\public" -Recurse -Force  # 删除 public 目录
 # 获取当前日期和时间，并格式化
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $commitMessage = "Update at $timestamp"
@@ -10,20 +14,14 @@ Write-Host "----------------------------------------"
 Write-Host "正在同步并部署网站..."
 Write-Host "提交信息: $commitMessage"
 Write-Host "----------------------------------------"
-
-# 同步远程仓库并 rebase，避免产生合并提交
-git pull --rebase
-
+git pull --rebase # 同步远程仓库并 rebase，避免产生合并提交
 # 提交所有本地修改
 git add .
 git commit -m $commitMessage
-
 # 推送本地修改到远程仓库
 git push
-
 Write-Host "----------------------------------------"
 Write-Host "部署完成"
 Write-Host "----------------------------------------"
-
-# 暂停，等待用户按键
+# 暂停
 Read-Host "回车键继续..."
